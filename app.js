9382c37163c25e2d73176f6291472fb3d2970bc4
Floriaen <floriaen@gmail.com>
2020-04-05 19:24:12 +0200

    Split Eyes, Nose and Mouth

--- app.js
+++ app.js
@@ -11,29 +11,37 @@ app.get('/random.png', function (req, res) {
   const ctx = canvas.getContext('2d');
    
   // Draw cat with lime helmet
-  loadImage(__dirname + '/data/images/eyes-and-mouth.png').then((eyesAndMouth) => {
-    loadImage(__dirname + '/data/images/shape.png').then((shape) => {
+  loadImage(__dirname + '/data/images/eyes.png').then((eyes) => {
+    loadImage(__dirname + '/data/images/mouth.png').then((mouth) => {
+      loadImage(__dirname + '/data/images/nose.png').then((nose) => {
+        loadImage(__dirname + '/data/images/shape.png').then((shape) => {
+          let w = 800;
+          let h = 800;
 
-      let w = 800;
-      let h = 800;
+          // face shape
+          let w1 = Math.floor(Math.random() * 3);
+          let h1 = Math.floor(Math.random() * 2);
+          ctx.drawImage(shape, w1 * w, h1 * h, w, h, 0, 0, 512, 512);
+          
+          // eyes
+          let w2 = Math.floor(Math.random() * 3);
+          let h2 = Math.floor(Math.random() * 2);
+          ctx.drawImage(eyes, w2 * w, h2 * h, w, h, 0, 0, 512, 512);
 
-      // face shape
-      let w1 = Math.floor(Math.random() * 3);
-      let h1 = Math.floor(Math.random() * 2);
-      ctx.drawImage(shape, w1 * w, h1 * h, w, h, 0, 0, 512, 512);
-      
-      // face eyes and mouth
-      let w2 = Math.floor(Math.random() * 3);
-      let h2 = Math.floor(Math.random() * 2);
-      ctx.drawImage(eyesAndMouth, w2 * w, h2 * h, w, h, 0, 0, 512, 512);
-
-      //res.set('Content-Type', 'text/html');
-      //res.send('<img src="' + canvas.toDataURL() + '" />');
-      
-      res.set('Content-Type', 'image/png');
-      //res.send(new Buffer(buffer, 'binary'))
-      res.send(canvas.toBuffer());
-      
+          // mouth
+          let w3 = Math.floor(Math.random() * 3);
+          let h3 = Math.floor(Math.random() * 2);
+          ctx.drawImage(mouth, w3 * w, h3 * h, w, h, 0, 0, 512, 512);
+  
+          // noise
+          let w4 = Math.floor(Math.random() * 3);
+          let h4 = Math.floor(Math.random() * 2);
+          ctx.drawImage(nose, w4 * w, h4 * h, w, h, 0, 0, 512, 512);
+  
+          res.set('Content-Type', 'image/png');
+          res.send(canvas.toBuffer());
+        })
+      })
     })
   })
     
