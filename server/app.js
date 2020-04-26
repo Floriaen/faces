ab7cfc8a440e82612b091e764349c86295e82c83
Floriaen <floriaen@gmail.com>
2020-04-26 12:52:20 +0200

    Add colorisation

--- server/app.js
+++ server/app.js
@@ -4,6 +4,46 @@ const seedrandom = require('seedrandom');
 const { createCanvas, loadImage } = require('canvas');
 const express = require('express');
 const app = express();
+const chroma = require('chroma-js');
+const palettes = require('nice-color-palettes');
+const humanSkinPalette = require('./humanSkinPalette.json');
+
+/*
+  image: image or canvas element
+  inlineColor: rgb array 
+  outlineColor: rgb array 
+*/
+function fillWithColor(image, inlineColor, outlineColor) {
+  let bufferCanvas = createCanvas(image.width, image.height);
+  let bufferCtx = bufferCanvas.getContext('2d');
+  bufferCtx.drawImage(image, 0, 0, image.width, image.height);
+  var imgData = bufferCtx.getImageData(0, 0, image.width, image.height);
+
+  for (i = 0; i < imgData.data.length; i += 4) {
+    if (imgData.data[i + 3] > 0) {
+      let r = imgData.data[i];
+      let g = imgData.data[i + 1];
+      let b = imgData.data[i + 2];
+      // calculates the luminance according * to Rec. 601 (PAL/NTSC) coefficients
+      let luminance = r * 0.299 + g * 0.587 + b * 0.114;
+      if (luminance < 200) {
+        //console.log(luminance);
+        imgData.data[i] = outlineColor[0];
+        imgData.data[i + 1] = outlineColor[1];
+        imgData.data[i + 2] = outlineColor[2];
+        imgData.data[i + 3] = 255;
+      } else {
+        imgData.data[i] = inlineColor[0];
+        imgData.data[i + 1] = inlineColor[1];
+        imgData.data[i + 2] = inlineColor[2];
+        imgData.data[i + 3] = 255;
+      }
+    }
+  }
+
+  bufferCtx.putImageData(imgData, 0, 0);
+  return bufferCanvas;
+}
 
 app.get('/face/:name', function (req, res) {
   // setup random seed from name:
@@ -11,10 +51,24 @@ app.get('/face/:name', function (req, res) {
   seed += seed.length.toString();
   random.use(seedrandom(seed));
 
-  let _size = config.get('Face.size');
+  // PALETTE initialise the colors used in the image:
+  let colors = {
+    background: null,
+    face: null,
+    mouth: chroma('pink')
+  };
+
+  let palette = palettes[random.int(0, 99)];
+  let bc = palette[random.int(0, 4)];
+  let bf = humanSkinPalette[random.int(0, humanSkinPalette.length - 1)];
+  colors.background = chroma(bc).luminance(0.09);
+  colors.face = chroma(bf).luminance(0.5);
+  colors.outline = colors.background.darken(1.6);
+
+  const _size = config.get('Face.size');
   const canvas = createCanvas(_size.width, _size.height);
   const ctx = canvas.getContext('2d');
-  ctx.fillStyle = '#FFF';
+  ctx.fillStyle = colors.background.hex();
   ctx.fillRect(0, 0, _size.width, _size.height);
 
   let _item = config.get('Face.item');
@@ -30,8 +84,10 @@ app.get('/face/:name', function (req, res) {
     let element = elements[1];
     let rw = random.int(0, _item.horizontalCount - 1);
     let rh = random.int(0, _item.verticalCount - 1);
-    ctx.drawImage(shape, rw * w, rh * h, w, h, 0, 0, _size.width, _size.height);
-    
+
+    var coloredShape = fillWithColor(shape, colors.face.rgb(), colors.outline.rgb());
+    ctx.drawImage(coloredShape, rw * w, rh * h, w, h, 0, 0, _size.width, _size.height);
+
     return loadImage(__dirname + element.directoryPath + 'eyes.png');
   })
   .then(eyes => {
@@ -46,22 +102,26 @@ app.get('/face/:name', function (req, res) {
     let element = elements[3];
     let rw = random.int(0, _item.horizontalCount - 1);
     let rh = random.int(0, _item.verticalCount - 1);
-    ctx.drawImage(mouth, rw * w, rh * h, w, h, 0, 0, _size.width, _size.height);
+
+    var coloredShape = fillWithColor(mouth, colors.mouth.rgb(), colors.outline.rgb());
+    ctx.drawImage(coloredShape, rw * w, rh * h, w, h, 0, 0, _size.width, _size.height);
 
     return loadImage(__dirname + element.directoryPath + 'nose.png');
   })
   .then(nose => {
     let rw = random.int(0, _item.horizontalCount - 1);
     let rh = random.int(0, _item.verticalCount - 1);
-    ctx.drawImage(nose, rw * w, rh * h, w, h, 0, 0, _size.width, _size.height);
+    
+    var coloredNose = fillWithColor(nose, colors.face.rgb(), colors.outline.rgb());
+    ctx.drawImage(coloredNose, rw * w, rh * h, w, h, 0, 0, _size.width, _size.height);
 
     res.set('Content-Type', 'image/png');
     res.send(canvas.toBuffer());
   }).catch(function(e) {
     console.log(e);
   });
-
 });
+
 app.listen(3000);
 
 function createRandomArrayFromElements(arrayLength, elements) {
